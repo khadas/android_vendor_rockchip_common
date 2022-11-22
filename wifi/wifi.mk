1368f7c16652666f8fe9688864b8006bcdcc11f8
Weiguo Hu <hwg@rock-chips.com>
2022-11-22 20:15:52 +0800

    wifi: install ko in external/wifi_driver/
    Signed-off-by: Weiguo Hu <hwg@rock-chips.com>
    Signed-off-by: Alex Zhao <zzc@rock-chips.com>
    Change-Id: I1a5560edf5a68fb02980af0659223b49e409a2fa

--- wifi/wifi.mk
+++ wifi/wifi.mk
@@ -1,5 +1,18 @@
-WIFI_KO_FILES := $(shell find $(TOPDIR)$(PRODUCT_KERNEL_PATH)/drivers/net/wireless/rockchip_wlan -name "*.ko" -type f)
+HAVE_EXT_WIFI_KO_FILE := $(shell test -d $(TOPDIR)external/wifi_driver/ && echo yes)
+ifeq ($(HAVE_EXT_WIFI_KO_FILE),yes)
+EXT_WIFI_KO_FILES := $(shell find $(TOPDIR)external/wifi_driver -name "*.ko" -type f)
+BOARD_VENDOR_KERNEL_MODULES += \
+        $(foreach file, $(EXT_WIFI_KO_FILES), $(file))
 
+# priority to use external/wifi_driver, delete the same ko in kernel wifi driver
+EXT_WIFI_DRIVER := $(shell find $(TOPDIR)external/wifi_driver -name "*.ko" -type f | awk -F "wifi_driver/" '{print $$2}' | awk -F "/" '{print $$1}')
+$(shell for line in $(EXT_WIFI_DRIVER); do rm $(TOPDIR)$(PRODUCT_KERNEL_PATH)/drivers/net/wireless/rockchip_wlan/$$line/*.ko > /dev/null 2>&1; done)
+
+$(shell for line in $(EXT_WIFI_DRIVER); do rm $(TOPDIR)$(PRODUCT_KERNEL_PATH)/drivers/net/wireless/rockchip_wlan/rkwifi/$$line/*.ko > /dev/null 2>&1; done)
+
+endif
+
+WIFI_KO_FILES := $(shell find $(TOPDIR)$(PRODUCT_KERNEL_PATH)/drivers/net/wireless/rockchip_wlan -name "*.ko" -type f)
 BOARD_VENDOR_KERNEL_MODULES += \
 	$(foreach file, $(WIFI_KO_FILES), $(file))
 
