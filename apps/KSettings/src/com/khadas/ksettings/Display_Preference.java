a40ef6dded9698995dcdf2f959903dd3fd0291cc
Goenjoy Huang <goenjoy@khadas.com>
2023-03-16 11:18:41 +0800

    KSettings: add support for setting rotation angle and screen density manually [1/2] frameworks/base [2/2]
    and fix setting the fan mode, restart cannot be saved
    
    Signed-off-by: Goenjoy Huang <goenjoy@khadas.com>

--- apps/KSettings/src/com/khadas/ksettings/Display_Preference.java
+++ apps/KSettings/src/com/khadas/ksettings/Display_Preference.java
@@ -1,34 +1,52 @@
 package com.khadas.ksettings;
 
-import android.os.Bundle;
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.os.SystemProperties;
 import android.preference.ListPreference;
 import android.preference.Preference;
 import android.preference.PreferenceActivity;
 import android.preference.PreferenceManager;
+import android.preference.PreferenceScreen;
+import android.preference.SwitchPreference;
+import android.provider.Settings;
+import android.widget.Toast;
 import android.util.Log;
 
 import java.io.IOException;
 
 
-public class LEDs_Preference extends PreferenceActivity implements Preference.OnPreferenceClickListener {
+public class Display_Preference extends PreferenceActivity implements Preference.OnPreferenceClickListener {
 
-	private Context mContext;
+    private ListPreference Screen_rotation_Preference;
+    private ListPreference Screen_density_Preference;
+
+    private static final String DISPLAY_ROTATION_KEY = "DISPLAY_ROTATION_KEY";
+    private static final String DISPLAY_DENSITY_KEY = "DISPLAY_DENSITY_KEY";
+
+
+    private static Context mContext;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        addPreferencesFromResource(R.xml.leds_control);
-		mContext = this;
+        addPreferencesFromResource(R.xml.display);
+
         getActionBar().setHomeButtonEnabled(true);
         getActionBar().setDisplayHomeAsUpEnabled(true);
 
-    }
+        mContext = this;
 
-    @Override
-    protected void onResume() {
-        super.onResume();
+        Screen_rotation_Preference = (ListPreference) findPreference(DISPLAY_ROTATION_KEY);
+        bindPreferenceSummaryToValue(Screen_rotation_Preference);
+		Screen_rotation_Preference.setOnPreferenceClickListener(this);
+
+        Screen_density_Preference = (ListPreference) findPreference(DISPLAY_DENSITY_KEY);
+        bindPreferenceSummaryToValue(Screen_density_Preference);
     }
 
     /**
@@ -46,6 +64,20 @@ public class LEDs_Preference extends PreferenceActivity implements Preference.On
                 int index = listPreference.findIndexOfValue(stringValue);
                 // Set the summary to reflect the new value.
                 preference.setSummary(index >= 0 ? listPreference.getEntries()[index] : null);
+
+                if (DISPLAY_DENSITY_KEY.equals(key)){
+                    String command = index >= 0 ? listPreference.getEntries()[index].toString() : "";
+                    try {
+                        ComApi.execCommand(new String[]{"sh", "-c", "wm density " + command});
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+
+                }else if(DISPLAY_ROTATION_KEY.equals(key)){
+                    Settings.System.putInt(mContext.getContentResolver(),"user_rotation",index);
+                    SystemProperties.set("persist.sys.user_rotation",""+index);
+                }
+
             }  else {
                 // For all other preferences, set the summary to the value's
                 // simple string representation.
@@ -72,8 +104,17 @@ public class LEDs_Preference extends PreferenceActivity implements Preference.On
 
     @Override
     public boolean onPreferenceClick(Preference preference) {
-		final String key = preference.getKey();
+        final String key = preference.getKey();
+        if (DISPLAY_ROTATION_KEY.equals(key)){
+            SystemProperties.set("set.user_rotation","true");
+        }
         return true;
     }
 
+    @Override
+    protected void onDestroy() {
+        // TODO Auto-generated method stub
+        super.onDestroy();
+        SystemProperties.set("set.user_rotation","false");
+    }
 }
